f03c97eb73f06f2eb23d5b7917ca2f6db4362ffa
Ventura Luz e Efeitos <joseleonardomcc@gmail.com>
2026-07-18 11:19:33 -0300

    Add Firebase SDK and configuration

---
+++ src/lib/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBEqhAeXJZfaAUts-QKVjhULr-czTKPTjM",
+  authDomain: "catolico-77369.firebaseapp.com",
+  projectId: "catolico-77369",
+  storageBucket: "catolico-77369.firebasestorage.app",
+  messagingSenderId: "836974565993",
+  appId: "1:836974565993:web:c1a97660ebe211444960f8",
+  measurementId: "G-D8VVB8T9YB"
+};
+
+const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
